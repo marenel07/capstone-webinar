c0c627066b504eac03f9bf598fc473213836f9f7
the-name-romel <racopra@sorsu.edu.ph>
2023-11-08 06:28:03 +0800

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Toaster } from "sonner";
 import AuthProvider from "@/providers/AuthProvider";
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Toaster richColors />
           <ModalProvider />
           {children}
+          <Analytics />
         </AuthProvider>
       </body>
     </html>
